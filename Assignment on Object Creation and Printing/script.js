e431ba7d0d2e33a930165e76f3fead35c5e5a069
KaranDalwaniPerficient <96812384+KaranDalwaniPerficient@users.noreply.github.com>
2022-01-11 19:16:40 +0530

    Update script.js

--- Assignment on Object Creation and Printing/script.js
+++ Assignment on Object Creation and Printing/script.js
@@ -1,12 +1,12 @@
 function display()
 {
     var form={
-        First_Name: document.getElementById("first_name").value,
-        Last_Name: document.getElementById("last_name").value,
-        Address: document.getElementById("address").value,
-        Blood_Group: document.getElementById("blood_group").value,
-        DOB: document.getElementById("dob").value,
-        Place_Of_Birth: document.getElementById("pob").value
+        firstName: document.getElementById("first_name").value,
+        lastName: document.getElementById("last_name").value,
+        address: document.getElementById("address").value,
+        bloodGroup: document.getElementById("blood_group").value,
+        dob: document.getElementById("dob").value,
+        placeOfBirth: document.getElementById("pob").value
     };
-    document.getElementById("infor").innerHTML +=("Name of user is"+form.First_Name+" "+form.Last_Name+" His/Her Address is "+form.Address+" His/Her Blood Group is "+form.Blood_Group+" His/Her Date of Birth is on "+form.DOB+"and his/her Place of Birth is "+form.Place_Of_Birth);
+    document.getElementById("infor").innerHTML +=("Name of user is"+form.firstName+" "+form.lastName+" His/Her Address is "+form.address+" His/Her Blood Group is "+form.bloodGroup+" His/Her Date of Birth is on "+form.dob+"and his/her Place of Birth is "+form.placeOfBirth);
 }

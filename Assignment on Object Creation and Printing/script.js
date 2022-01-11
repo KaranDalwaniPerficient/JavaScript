ca569f6fa91e6dc23e15c89da3f820c666ae14b7
KaranDalwaniPerficient <96812384+KaranDalwaniPerficient@users.noreply.github.com>
2022-01-11 19:39:19 +0530

    Update script.js

--- Assignment on Object Creation and Printing/script.js
+++ Assignment on Object Creation and Printing/script.js
@@ -9,4 +9,5 @@ function display()
         placeOfBirth: document.getElementById("pob").value
     };
     document.getElementById("infor").innerHTML +=("Name of user is "+form.firstName+" "+form.lastName+" His/Her Address is "+form.address+" His/Her Blood Group is "+form.bloodGroup+" His/Her Date of Birth is on "+form.dob+"and his/her Place of Birth is "+form.placeOfBirth);
+    console.log("Name of user is "+form.First_Name+" "+form.Last_Name+" His/Her Address is "+form.Address+" His/Her Blood Group is "+form.Blood_Group+" His/Her Date of Birth is on "+form.DOB+" and his/her Place of Birth is "+form.Place_Of_Birth);
 }

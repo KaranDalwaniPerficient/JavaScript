e37d8188078ca9a15422383718144835045c343c
KaranDalwaniPerficient <96812384+KaranDalwaniPerficient@users.noreply.github.com>
2022-01-11 19:32:01 +0530

    Update script.js

--- Assignment on Object Creation and Printing/script.js
+++ Assignment on Object Creation and Printing/script.js
@@ -8,5 +8,5 @@ function display()
         dob: document.getElementById("dob").value,
         placeOfBirth: document.getElementById("pob").value
     };
-    document.getElementById("infor").innerHTML +=("Name of user is"+form.firstName+" "+form.lastName+" His/Her Address is "+form.address+" His/Her Blood Group is "+form.bloodGroup+" His/Her Date of Birth is on "+form.dob+"and his/her Place of Birth is "+form.placeOfBirth);
+    document.getElementById("infor").innerHTML +=("Name of user is "+form.firstName+" "+form.lastName+" His/Her Address is "+form.address+" His/Her Blood Group is "+form.bloodGroup+" His/Her Date of Birth is on "+form.dob+"and his/her Place of Birth is "+form.placeOfBirth);
 }

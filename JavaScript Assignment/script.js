6fe216400b7e77d0fd12b1a0aac0df7e435fc42c
KaranDalwaniPerficient <96812384+KaranDalwaniPerficient@users.noreply.github.com>
2022-05-22 23:51:16 +0530

    Add files via upload

---
+++ JavaScript Assignment/script.js
@@ -0,0 +1,25 @@
+let num1 = document.getElementById("num1");
+let num2 = document.getElementById("num2");
+let result = document.getElementById("res");
+document.getElementById("clrs").onclick = () =>
+{
+    document.getElementById("num1").value="";
+    document.getElementById("num2").value="";
+    document.getElementById("res").value="";
+}
+document.getElementById("addition").onclick = () =>
+{
+    result.value = parseInt(num1.value) + parseInt(num2.value);
+}
+document.getElementById("subtraction").onclick = () =>
+{
+    result.value = parseInt(num1.value) - parseInt(num2.value);
+}
+document.getElementById("division").onclick = () =>
+{
+    result.value = parseInt(num1.value) / parseInt(num2.value);
+}
+document.getElementById("mod").onclick = () =>
+{
+    result.value = parseInt(num1.value) % parseInt(num2.value);
+}
